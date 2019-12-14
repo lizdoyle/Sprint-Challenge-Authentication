5542e561d610be387f2584bef36f3f80d3cd78d3
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-13 22:49:09 -0500

    updating token usage

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,11 +3,30 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets');
+
+
 module.exports = (req, res, next) => {
-  console.log(req.session);
-  if (req.session && req.session.username) {
+
+  const token = req.headers.authorization;
+
+  if (req.decodedJwt) {
     next();
-  } else {
-      res.status(401).json({ you: "shall not pass!" });
-  }
-};
+  } else if{
+      jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+          if (err) {
+                res.status(401).json({message: "You shall not pass!"})
+            }
+        else {
+            req.decodedJwt = decodedJwt;
+            next()
+               }
+           })
+          } 
+        else {
+            req.status(401).json({message: "invalid token or no token provided"})
+        }
+     
+}
