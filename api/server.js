ce7f44129ee4d822c0c0bb0669f2fbcb4efa88d9
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-13 21:44:19 -0500

    start on server

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const session = require('express-session');
+
+
 const cors = require('cors');
 const helmet = require('helmet');
 
@@ -8,11 +11,32 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'lizdoyle',
+    secret: "backendisthesecret",
+
+    cookie: {
+        maxAge: 1000 * 60,
+        secure: false,
+        httpOnly: true,
+    },
+
+    resave: false,
+    saveUninitialized: false
+};
+
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use(session(sessionConfig));
+
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+    res.json({api: "This is working!"})
+});
 
 module.exports = server;
